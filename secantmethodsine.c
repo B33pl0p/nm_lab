45d0785a1af3c1b27bd8e63f2c11012c2d5f73f0
B33pl0p <biplop.077bei013@acem.edu.np>
2022-12-19 11:14:03 +0545

    day 3 labs

---
+++ secantmethodsine.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+float func(float x){
+    return 4*sin(x)-exp(x);
+}
+int main(){
+    float x0,x1,x2,e;
+    int count=1;
+    printf("Enter the value of x0,x1,e : ");
+    scanf("%f%f%f",&x0,&x1,&e);
+    while(1){
+    count++;
+    x2 = x1-(((x1-x0)*func(x1))/(func(x1)-func(x0)));
+    x0=x1;
+    x1=x2;
+    if(fabs((x1-x0)/x1)<e)
+        break;
+    }
+printf("The root is %f",x2);
+printf("\nThe number of iteration is %d",count);
+}
